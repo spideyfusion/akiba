beb71606b6e6c688f062efb2d5c69cd30316d639
Petar Obradović <spideyfusion@gmail.com>
2018-03-31 06:25:57 +0200

    Updated copyright metadata.

--- Akiba/Properties/AssemblyInfo.cs
+++ Akiba/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AkibaInterceptor")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
